8078972bca86f2391843cdfefdedd322f86f8ba6
SALONI SINGH <87686940+Slonysingh@users.noreply.github.com>
2025-02-20 10:48:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+var num = 20;
+var num = 30;
+console.log(num); 
+
+let num1 = 50;
+num1 = 60;
+console.log(num1)
+
+const num2 = 40;
+// num2 = 70;
+console.log(num2);
+
+
+// keywords or variables //
+// let
+// var
+// const
